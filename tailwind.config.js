6686e0c9779f1356e5aabd427671e652f6a17693
takeru.dev <takeru.konno.cw`gmail.com>
2022-12-15 19:20:51 +0900

    ➕ UIコンポーネントを使うためにMantineを導入した #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    // Mantineのブレークポイントと一致させる
+    screens: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
