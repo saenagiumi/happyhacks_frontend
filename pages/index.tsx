cc6f54ef91f0d07e4fc4c7195f0348c4160793b9
takeru.dev <takeru.konno.cw`gmail.com>
2022-12-16 11:52:11 +0900

    ✨ 課題の投稿一覧表示機能を実装した #2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,11 @@
-import useSWR from "swr";
 import { Header } from "../components/Header/Header";
-
-const fetcher = async (url) => {
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-
-const usePosts = () => {
-  const { data, error } = useSWR(
-    "https://jsonplaceholder.typicode.com/posts",
-    fetcher
-  );
-  return {
-    data,
-    error
-  };
-};
+import { PostList } from "../components/Post/PostList";
 
 export default function Home() {
-  const { data } = usePosts();
-
   return (
     <div>
       <Header />
-      <ol>
-        {data?.map((post) => {
-          return <li key={post.id}>{post.title}</li>;
-        })}
-      </ol>
+      <PostList />
     </div>
   );
 }
