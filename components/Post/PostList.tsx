467e97b84e08c6e9e53c9b855298f516af453184
takeru.dev <takeru.konno.cw`gmail.com>
2022-12-17 09:18:42 +0900

    ✨ 課題一覧から課題詳細へアクセスができるようにした #3

--- components/Post/PostList.tsx
+++ components/Post/PostList.tsx
@@ -1,25 +1,24 @@
-// import { usePosts } from "hooks/usePosts";
+import Link from "next/link";
 import { Post } from "components/Post/Post";
 import { useFetchArray } from "hooks/useFetchArray";
 import { API_URL } from "utils/const";
 
 export const PostList = () => {
   const { data, error, isLoading, isEmpty } = useFetchArray(`${API_URL}/posts`);
-  console.log(data);
-  
 
   return (
     <ol className="mx-2">
       {data?.map((post) => {
-        console.log(post.id);
         return (
           <li key={post.id} className="mb-2">
-            <Post
-              title={post.title}
-              body={post.body}
-              author={post.author}
-              postedAt={"2022/12/09 22:56"}
-            />
+            <Link href={`/posts/${post.id}`} className="no-underline">
+              <Post
+                title={post.title}
+                body={post.body}
+                author={post.author}
+                postedAt={"2022/12/09 22:56"}
+              />
+            </Link>
           </li>
         );
       })}
