73e02324d5d4b917850757efc9e8ae8130e66b1d
takeru.dev <takeru.konno.cw`gmail.com>
2022-12-17 12:28:05 +0900

    🎨 課題詳細ページでは課題一覧と同じコンポーネントを使い回すようにした #3

--- components/Post/PostDetail.tsx
+++ components/Post/PostDetail.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useFetch } from "hooks/useFetch";
 import { API_URL } from "utils/const";
+import { Post } from "./Post";
 
 export const PostDetail = () => {
   const router = useRouter();
@@ -22,12 +23,14 @@ export const PostDetail = () => {
       <Head>
         <title>{data?.title}</title>
       </Head>
-
-      <h1 className="text-3xl font-bold">{data?.title}</h1>
-      <p className="text-xl text-gray-900 mt-2">{data?.body}</p>
-
-      <h2 className="text-xl font-bold mt-10">コメント一覧</h2>
-      <div className="mt-2"></div>
+      <div>
+        <Post
+          title={data.title}
+          body={data.body}
+          author={data.author}
+          postedAt={"2022/12/09 22:56"}
+        />
+      </div>
     </div>
   );
 };
