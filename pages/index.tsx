704c2d02a82d4dbbaf13fdc45c917c1a6c77ce55
takeru.dev <takeru.konno.cw`gmail.com>
2022-12-17 09:22:39 +0900

    🏗️ ヘッダーは全てのページで共通にしたいコンポーネントなので_appに移動した #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-import { Header } from "components/Header/Header";
 import { PostList } from "components/Post/PostList";
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <PostList />
     </div>
   );
