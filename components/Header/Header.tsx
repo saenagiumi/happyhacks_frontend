89728bc3064ef33f9ed2a002af12ad6c1640f4f6
takeru.dev <takeru.konno.cw`gmail.com>
2022-12-22 18:28:49 +0900

    ✨ ロゴにトップページへのリンクを追加した

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,9 @@ import { Button } from "@mantine/core";
 export const Header = () => {
   return (
     <div className="h-[100px] flex justify-between items-center pr-5">
+      <Link href="/">
         <Logo className="w-[200px] h-full" />
+      </Link>
       <div>
         <ul>
           <Link href="/posts/create">
