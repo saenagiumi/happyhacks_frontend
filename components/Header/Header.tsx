080f59697df1319e87fced562709f7f93f6a740c
takeru.dev <takeru.konno.cw`gmail.com>
2022-12-22 18:28:01 +0900

    ✨ 新規投稿作成ページへのリンクを追加した #4

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import Logo from "../../public/logo.svg";
+import Link from "next/link";
+import { Button } from "@mantine/core";
 
 export const Header = () => {
   return (
     <div className="h-[100px] flex justify-between items-center pr-5">
-      <Logo className="w-[200px] h-full" />
+        <Logo className="w-[200px] h-full" />
       <div>
         <ul>
-          <li className="px-3 py-2 bg-yellow-400 rounded">ログイン</li>
+          <Link href="/posts/create">
+            <Button color="yellow" size="xs">
+              質問する
+            </Button>
+          </Link>
         </ul>
       </div>
     </div>
