9555779503bb76216e2d2369e5f3bf63b2c82162
takeru.dev <takeru.konno.cw`gmail.com>
2023-01-11 17:14:35 +0900

    ✨ コメント一覧を実際のデータを取得して表示するように実装した #8

---
+++ components/Comment/CommentListByPostId.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { useFetchArray } from "hooks/useFetchArray";
+import { API_URL } from "utils/const";
+import { Comment } from "components/Comment/Comment";
+
+export const CommentListByPostId = (props: { id: string }) => {
+  const { data, error, isLoading, isEmpty } = useFetchArray(
+    `${API_URL}/posts/${props.id}/comments`
+  );
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  if (isEmpty) {
+    return <p className=" text-gray-500">あなたの知見を共有してあげましょう</p>;
+  }
+
+  return (
+      <ol className="space-y-2">
+        {data?.map((comment: any) => {
+          console.log(comment);
+          
+          return (
+            <li key={comment.id}>
+              <Comment
+                title={comment.title}
+                body={comment.body}
+                userId={comment.user_id}
+                postedAt={comment.created_at}
+              />
+            </li>
+          );
+        })}
+      </ol>
+  );
+};
--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -1,25 +1,37 @@
 import { Comment } from "components/Comment/Comment";
 import { PostDetail } from "components/Post/PostDetail";
 
+import { useFetch } from "hooks/useFetch";
+import { useRouter } from "next/router";
+import { API_URL } from "utils/const";
+
 import { Modal, Avatar } from "@mantine/core";
 import { useState } from "react";
 import CommentForm from "components/Comment/CommentForm";
+import { CommentListByPostId } from "components/Comment/CommentListByPostId";
 
 const PostsId = () => {
+  const router = useRouter();
+
+  const { data, error, isLoading } = useFetch(
+    router.query.id ? `${API_URL}/posts/${router.query.id}` : null
+  );
+
   const [opened, setOpened] = useState(false);
+
+  if (isLoading) {
+    return <div>ローディング中</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
 
   return (
     <div className="mx-2">
       <PostDetail />
-      <h2 className="font-medium text-gray-600 my-4 ml-1">２件の提案</h2>
-      <Comment
-        title={"次の日の持ち物をメモした紙を次の日履く靴の中に入れておく"}
-        body={
-          "タイトルの通りです。持ち物リストを自分の生活動線の中で必ず目に触れるところに置いておくと、嫌でも忘れなくなります。ただ、そのまま紙だと気づかずに靴を履いてしまう恐れがあるので、小さめのメモが書けるホワイトボードなどそういうものを置いてもいいかもしれませんね！一太郎スマイルさんの生活がより良くなることをお祈りしています！！ファイト！"
-        }
-        author={"花咲か爺さん"}
-        postedAt={"2022/12/09 22:56"}
-      />
+      <h2 className="font-medium text-gray-600 my-4 ml-1">２件の提案</h2>   
+      <CommentListByPostId id={data.id} />
       <Modal centered opened={opened} onClose={() => setOpened(false)}>
         <CommentForm />
       </Modal>
