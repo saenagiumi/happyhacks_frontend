d90b1d7e3dde18464410184105b15f7867ad4db2
takeru.dev <takeru.konno.cw`gmail.com>
2023-01-12 10:52:41 +0900

    ✨ モーダルの開閉操作が行えるようにした #8

--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -1,23 +1,21 @@
-import { Comment } from "components/Comment/Comment";
 import { PostDetail } from "components/Post/PostDetail";
 
 import { useFetch } from "hooks/useFetch";
 import { useRouter } from "next/router";
 import { API_URL } from "utils/const";
 
+import { useDisclosure } from "@mantine/hooks";
 import { Modal, Avatar } from "@mantine/core";
-import { useState } from "react";
 import CommentForm from "components/Comment/CommentForm";
 import { CommentListByPostId } from "components/Comment/CommentListByPostId";
 
 const PostsId = () => {
   const router = useRouter();
+  const [opened, modalHandlers] = useDisclosure(false);
 
   const { data, error, isLoading } = useFetch(
     router.query.id ? `${API_URL}/posts/${router.query.id}` : null
   );
-
-  const [opened, setOpened] = useState(false);
 
   if (isLoading) {
     return <div>ローディング中</div>;
@@ -30,16 +28,16 @@ const PostsId = () => {
   return (
     <div className="mx-2">
       <PostDetail />
-      <h2 className="font-medium text-gray-600 my-4 ml-1">２件の提案</h2>   
+      <h2 className="font-medium text-gray-600 my-4 ml-1">２件の提案</h2>
       <CommentListByPostId id={data.id} />
-      <Modal centered opened={opened} onClose={() => setOpened(false)}>
-        <CommentForm />
+      <Modal centered opened={opened} onClose={() => modalHandlers.close()}>
+        <CommentForm modalHandlers={modalHandlers} />
       </Modal>
 
       <div className="flex mt-4">
-        <Avatar radius="xl" size="md"/>
+        <Avatar radius="xl" size="md" />
         <div
-          onClick={() => setOpened(true)}
+          onClick={() => modalHandlers.open()}
           className="w-full box-border ml-2 p-3 border-solid border border-gray-300 rounded-lg  text-gray-400"
         >
           回答する
