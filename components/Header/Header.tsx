1c11901bc963964ce3ef827c63a052545c7dca7d
takeru.dev <takeru.konno.cw`gmail.com>
2023-01-26 18:28:49 +0900

    ✨ ログイン状態に応じてヘッダー内の項目を出し分ける用意にした #34

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,22 +1,84 @@
 import Logo from "../../public/logo.svg";
 import Link from "next/link";
-import { Button } from "@mantine/core";
+import { Avatar, Button, Menu } from "@mantine/core";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Header = () => {
+  const { user, isLoading, loginWithRedirect, logout } = useAuth0();
+
+  const handleLogout = () => {
+    // RecoilでsessionStorageに保存したアクセストークンを消去
+    sessionStorage.clear();
+    logout();
+  };
+
   return (
     <div className="h-[100px] flex justify-between items-center pr-5">
       <Link href="/">
         <Logo className="w-[200px] h-full" />
       </Link>
-      <div>
-        <ul>
-          <Link href="/posts/new">
-            <Button color="yellow" size="xs">
-              質問する
+
+      {user === undefined && !isLoading && (
+        // ログアウト時の表示
+        <ul className="flex items-center">
+          <li className="mr-3">
+            <Button
+              variant="subtle"
+              color="yellow"
+              size="xs"
+              radius="xs"
+              onClick={() => loginWithRedirect()}
+            >
+              ログイン
             </Button>
-          </Link>
+          </li>
+          <li>
+            <Button
+              color="yellow"
+              size="xs"
+              radius="sm"
+              onClick={() =>
+                loginWithRedirect({
+                  authorizationParams: {
+                    screen_hint: "signup",
+                  },
+                })
+              }
+            >
+              新規登録
+            </Button>
+          </li>
         </ul>
-      </div>
+      )}
+
+      {user && (
+        // ログイン時の表示
+        <ul className="flex items-center">
+          <li className="mr-3">
+            <Menu shadow="md" width={200}>
+              <Menu.Target>
+                <Avatar radius="xl" size="md" src={user.picture} />
+              </Menu.Target>
+
+              <Menu.Dropdown>
+                <Menu.Item>マイページ</Menu.Item>
+                <Menu.Item>お気に入り</Menu.Item>
+
+                <Menu.Divider />
+
+                <Menu.Item onClick={() => handleLogout()}>ログアウト</Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </li>
+          <li>
+            <Link href="/posts/new">
+              <Button color="yellow" size="xs">
+                質問する
+              </Button>
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import { useSetRecoilState } from "recoil";
 import tokenState from "recoil/atoms/tokenState";
 
 export default function Home() {
-  const { loginWithRedirect, logout, getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
   const setToken = useSetRecoilState(tokenState);
 
   // ログイン時にトークンを取得しRecoilへ格納
@@ -27,16 +27,8 @@ export default function Home() {
     getToken();
   }, []);
 
-  const handleLogout = () => {
-    // RecoilでsessionStorageに保存したアクセストークンを消去
-    sessionStorage.clear();
-    logout();
-  };
-
   return (
     <div>
-      <button onClick={() => loginWithRedirect()}>ログイン</button>
-      <button onClick={() => handleLogout()}>ログアウト</button>
       <PostList />
     </div>
   );
