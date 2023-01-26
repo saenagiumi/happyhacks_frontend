3c455d2c46b3a71aeeb130e87b8ce4ee08194f34
takeru.dev <takeru.konno.cw`gmail.com>
2023-01-26 09:13:43 +0900

    ✨ 認証機能を導入し、アクセストークンを使用してPostを送るようにした #32

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,15 @@ import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { Header } from "components/Header/Header";
 
+// 認証
+import { Auth0Provider } from "@auth0/auth0-react";
+
+// 状態管理
+import { RecoilRoot } from "recoil";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const redirectUri = `${process.env["NEXT_PUBLIC_BASE_URL"]}`;
+
   return (
     <MantineProvider
       withGlobalStyles
@@ -11,7 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
       theme={{
         /** Put your mantine theme override here */
         colorScheme: "light",
-        fontFamily: 'Noto Sans JP, sans-serif',
+        fontFamily: "Noto Sans JP, sans-serif",
         fontSizes: {
           xs: 12,
           sm: 14,
@@ -19,11 +27,21 @@ export default function App({ Component, pageProps }: AppProps) {
           lg: 16,
           xl: 20,
         },
-        
       }}
     >
-      <Header />
-      <Component {...pageProps} />
+      <Auth0Provider
+        domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"]!}
+        clientId={process.env["NEXT_PUBLIC_AUTH0_CLIENT_ID"]!}
+        authorizationParams={{
+          redirect_uri: redirectUri,
+          audience: process.env["NEXT_PUBLIC_AUTH0_AUDIENCE"]!,
+        }}
+      >
+        <RecoilRoot>
+          <Header />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </Auth0Provider>
     </MantineProvider>
   );
 }
