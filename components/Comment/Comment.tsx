2288b2c55892c88b25b4ca1fa16445ae2f43ed42
takeru.dev <takeru.konno.cw`gmail.com>
2023-02-09 16:37:20 +0900

    💄 ✨ いいねボタンの実装と日付のフォーマットやスタイルを色々と調整した #50

--- components/Comment/Comment.tsx
+++ components/Comment/Comment.tsx
@@ -1,3 +1,6 @@
+import { useCallback, useState } from "react";
+
+// Mantine
 import {
   createStyles,
   Text,
@@ -7,7 +10,21 @@ import {
   Paper,
   Button,
 } from "@mantine/core";
-import dayjs from "dayjs";
+
+// Day.js
+import dayjs from 'dayjs';
+
+// 日本語対応
+import 'dayjs/locale/ja';
+dayjs.locale('ja');
+
+// 相対日時のプラグイン
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
+// お気に入りボタン
+import { LikeButton } from "./LikeButton";
+
 
 const useStyles = createStyles((theme) => ({
   comment: {
@@ -15,8 +32,8 @@ const useStyles = createStyles((theme) => ({
   },
 
   body: {
-    paddingLeft: 7,
-    paddingTop: 8,
+    paddingLeft: 5,
+    paddingTop: 5,
     paddingBottom: 5,
     fontSize: theme.fontSizes.sm,
   },
@@ -36,50 +53,63 @@ interface CommentProps {
   postedAt: string;
 }
 
-export const Comment = ({ title, body, name, iconSrc, postedAt }: CommentProps) => {
+export const Comment = ({
+  title,
+  body,
+  name,
+  iconSrc,
+  postedAt,
+}: CommentProps) => {
+  const [on, setOn] = useState(false);
+  const handleClick = useCallback(() => {
+    setOn((prev) => !prev);
+  }, []);
+
   const { classes } = useStyles();
   return (
-    <Paper
-      shadow="sm"
-      radius="xs"
-      p="xs"
-      withBorder
-      className={classes.comment}
-    >
-      <Group className="my-2" position="apart">
-        <Group spacing="xs" className="px-1">
-          <Avatar src={iconSrc} alt={name} radius="xl" size="md" />
-          <Text className="text-gray-600" size="lg">
-            <div>{name}</div>
-          </Text>
+    <div>
+      <Paper
+        shadow="sm"
+        radius="xs"
+        p="xs"
+        withBorder
+        className={classes.comment}
+      >
+        <Group className="mb-2 pt-1" position="apart">
+          <Group spacing="xs" className="px-2">
+            <Avatar src={iconSrc} alt={name} radius="xl" />
+            <Text className="pt-0.5" size="sm">
+              <div className="text-gray-700 font-bold">{name}</div>
+              <div className="text-gray-400 text-xs">{dayjs(postedAt).fromNow()}</div>
+            </Text>
+          </Group>
+          <div className="mr-1 flex justify-center items-center">
+            <div
+              onClick={handleClick}
+              className="flex items-center rounded-none border-solid border-gray-100"
+            >
+              <LikeButton on={on}></LikeButton>
+              <div className=" mt-[3px] mr-3.5 text-gray-400">2</div>
+            </div>
+          </div>
         </Group>
-        <Group className="pr-2">
-          <Text size="sm" color="dimmed">
-            {dayjs(postedAt).format("MM/DD HH:mm")}
-          </Text>
-        </Group>
-      </Group>
-      <Text className="pl-2 pb-3 text-gray-600 font-bold" size="xl">
-        {title}
-      </Text>
+        <Text className="pl-1.5 pb-3 text-gray-600 font-bold" size="xl">
+          {title}
+        </Text>
 
-      <TypographyStylesProvider className={classes.body}>
-        <div className="w-full break-all text-gray-500 text-base">{body}</div>
-      </TypographyStylesProvider>
-      <Group className="my-5" position="center">
-        <Button
-          variant="gradient"
-          gradient={{ from: "#ed6ea0", to: "#ec8c69", deg: 35 }}
-        >
-          やりたい！28
-        </Button>
-        <Button
-          variant="gradient"
-          gradient={{ from: "teal", to: "cyan", deg: 60 }}
-        >
-          リストに追加
-        </Button>
-      </Group>
-    </Paper>
+        <TypographyStylesProvider className={classes.body}>
+          <div className="w-full break-all text-gray-500 text-base">{body}</div>
+        </TypographyStylesProvider>
+
+        <Group className="my-5 mx-3" position="center">
+          <Button
+            // variant="gradient"
+            // gradient={{ from: "teal", to: "cyan", deg: 60 }}
+          >
+            リストに追加
+          </Button>
+        </Group>
+      </Paper>
+    </div>
   );
 };
