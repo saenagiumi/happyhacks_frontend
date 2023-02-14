c3fe3be3868c42837f6dcd7fb20dec4bcce0fd32
takeru.dev <takeru.konno.cw`gmail.com>
2023-02-14 17:56:28 +0900

    💄 ヘッダー周りのスタイルを調整した #50

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,11 @@
-import Logo from "../../public/logo.svg";
+import Logo from "../../public/header-logo.svg";
 import Link from "next/link";
-import { Avatar, Button, Menu } from "@mantine/core";
+import { Avatar, Button, Menu, UnstyledButton } from "@mantine/core";
 import { useAuth0 } from "@auth0/auth0-react";
+
+import { HiOutlinePencilAlt } from "react-icons/hi";
+import { MdNotificationsNone } from "react-icons/md";
+
 
 export const Header = () => {
   const { user, isLoading, loginWithRedirect, logout } = useAuth0();
@@ -13,18 +17,18 @@ export const Header = () => {
   };
 
   return (
-    <div className="h-[100px] flex justify-between items-center pr-5">
+    <div className="h-12 flex justify-between items-center pt-1 pl-2 pr-2.5">
       <Link href="/">
-        <Logo className="w-[200px] h-full" />
+        <Logo className="w-[170px] h-full" />
       </Link>
 
       {user === undefined && !isLoading && (
         // ログアウト時の表示
         <ul className="flex items-center">
-          <li className="mr-3">
+          <li className="mr-1">
             <Button
               variant="subtle"
-              color="yellow"
+              color="green.4"
               size="xs"
               radius="xs"
               onClick={() => loginWithRedirect()}
@@ -34,7 +38,7 @@ export const Header = () => {
           </li>
           <li>
             <Button
-              color="yellow"
+              color="green.4"
               size="xs"
               radius="sm"
               onClick={() =>
@@ -54,15 +58,18 @@ export const Header = () => {
       {user && (
         // ログイン時の表示
         <ul className="flex items-center">
+          <li className="flex text-gray-400 mr-3">
+            <MdNotificationsNone size={22} />
+          </li>
           <li className="mr-3">
             <Menu shadow="md" width={200}>
               <Menu.Target>
-                <Avatar radius="xl" size="md" src={user.picture} />
+                <Avatar radius="xl" size={32} src={user.picture} />
               </Menu.Target>
 
               <Menu.Dropdown>
-                <Menu.Item>マイページ</Menu.Item>
-                <Menu.Item>お気に入り</Menu.Item>
+                <Menu.Item>プロフィール</Menu.Item>
+                <Menu.Item>マイリスト</Menu.Item>
 
                 <Menu.Divider />
 
@@ -72,9 +79,9 @@ export const Header = () => {
           </li>
           <li>
             <Link href="/posts/new">
-              <Button color="yellow" size="xs">
-                質問する
-              </Button>
+              <UnstyledButton className="flex items-center justify-center w-[32px] h-[31px] text-emerald-50 bg-main-green rounded-[3px]">
+                  <HiOutlinePencilAlt size={18} />
+              </UnstyledButton>
             </Link>
           </li>
         </ul>
