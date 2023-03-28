41ef750bbf20afd3b9c6f7654d417cae9cc40487
takeru.dev <takeru.konno.cw`gmail.com>
2023-03-28 10:32:48 +0900

    🎨 [Update]: 型定義を切り出し #142

--- features/posts/types/index.ts
+++ features/posts/types/index.ts
@@ -1,9 +1,27 @@
-export type Post = {
-  id: string;
+export type PostType = {
+  id: number;
   title: string;
   body: string;
   name: string;
   user_id: string;
   picture: string;
   created_at: string;
+};
+
+export type PostWithCommentsCountType = {
+  id: number;
+  title: string;
+  body: string;
+  name: string;
+  user_id: number;
+  picture: string;
+  created_at: string;
+  comments_count: number;
+};
+
+
+export type TargetPost = {
+  id: string;
+  title: string;
+  body: string;
 };
