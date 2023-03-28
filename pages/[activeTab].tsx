c28a5cfbac711bd222456cf78664fdf846f72515
takeru.dev <takeru.konno.cw`gmail.com>
2023-03-28 16:50:47 +0900

    💄 [Update]: スタイルを調整

--- pages/[activeTab].tsx
+++ pages/[activeTab].tsx
@@ -36,8 +36,8 @@ export const ActiveTab = () => {
                   <div className="flex">
                     <Link href={"/about"} legacyBehavior>
                       <UnstyledButton className="flex justify-end items-center group text-[1.1rem] xs:text-[1.1rem] md:text-[1.2rem] xs:w-[300px] h-[3.5rem] md:h-[3.5rem] text-gray-700 font-sans font-[600] border-[2.5px] border-solid border-amber-300 rounded-full bg-white hover:bg-amber-300 w-[78vw]  md:w-[300px] pl-5 md:pl-6 pr-1 md:pr-[5px] mx-auto">
-                        <p className="mx-auto">HappyHacksについて</p>
-                        <span className="px-3.5 py-3 mr-0.5 md:px-3 md:py-2.5 rounded-full text-white bg-amber-400 group-hover:text-gray-700">
+                        <p className="mx-auto xs:pr-2">HappyHacksについて</p>
+                        <span className="px-2.5 py-2 mr-0.5 rounded-full text-white bg-amber-400 group-hover:text-gray-700">
                           →
                         </span>
                       </UnstyledButton>
