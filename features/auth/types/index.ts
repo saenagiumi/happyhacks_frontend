a19a96ba856d76212768565e037b02deead01252
takeru.dev <takeru.konno.cw`gmail.com>
2023-03-28 10:38:57 +0900

    🏗️ [Update]: 型定義を切り出し #142

---
+++ features/auth/types/index.ts
@@ -0,0 +1,13 @@
+export type UserPostType = {
+  postData: {
+    sub: string | undefined;
+    name: string | undefined;
+    picture: string | undefined;
+  };
+};
+
+export type UserPostData = {
+  sub: string | undefined;
+  name: string | undefined;
+  picture: string | undefined;
+}
