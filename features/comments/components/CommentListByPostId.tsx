7d2bf24db1648d7dd364995ee92bc67e8b91ae2f
takeru.dev <takeru.konno.cw`gmail.com>
2023-03-28 16:05:11 +0900

    💄 [Update]: スタイルを調整

--- features/comments/components/CommentListByPostId.tsx
+++ features/comments/components/CommentListByPostId.tsx
@@ -51,10 +51,10 @@ const CommentListByPostId = (props: Props) => {
       {!isLoading && user === undefined && (
         <div className="flex justify-center mt-2.5 mb-5">
           <Button
-            className="text-emerald-50 font-sans text-[15px] h-[38px] px-[24.6px]"
+            className="text-emerald-50 font-sans mr-0.2 text-[15px] h-[38px] px-[25px]"
             onClick={() => loginWithPopup()}
             color="green.4"
-            // size="sm"
+            size="sm"
             radius={4}
           >
             ログインして回答する
