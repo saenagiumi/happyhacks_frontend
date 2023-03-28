e6d1253b5aa4fb4ebd807e4316181f23a9e9117e
takeru.dev <takeru.konno.cw`gmail.com>
2023-03-28 17:00:48 +0900

    💄 [Update]: スタイルを調整

--- pages/[activeTab].tsx
+++ pages/[activeTab].tsx
@@ -29,15 +29,15 @@ export const ActiveTab = () => {
               <div className="flex-col sm:w-[50%] flex justify-center items-center">
                 <div className="flex-col justify-center items-center">
                   <h1 className="font-sans inline-block font-[500] text-[7.3vw] xs:text-[2.1rem] md:text-[2.5rem] text-center leading-tight mb-5">
-                    <span className="inline-block">ADHDの日常生活の困難</span>
-                    <span className="inline-block">を環境調整でハックする</span>
+                    <span className="inline-block font-sans">ADHDの日常生活の困難</span>
+                    <span className="inline-block font-sans">を環境調整でハックする</span>
                   </h1>
 
                   <div className="flex">
                     <Link href={"/about"} legacyBehavior>
-                      <UnstyledButton className="flex justify-end items-center group text-[1.1rem] xs:text-[1.1rem] md:text-[1.2rem] xs:w-[300px] h-[3.5rem] md:h-[3.5rem] text-gray-700 font-sans font-[600] border-[2.5px] border-solid border-amber-300 rounded-full bg-white hover:bg-amber-300 w-[78vw]  md:w-[300px] pl-5 md:pl-6 pr-1 md:pr-[5px] mx-auto">
+                      <UnstyledButton className="flex justify-end items-center group text-[1.1rem] xs:text-[1.1rem] md:text-[1.2rem] xs:w-[300px] h-[3.5rem] md:h-[3.5rem] text-gray-700 font-sans font-[600] border-2 border-solid border-amber-300 rounded-full bg-white hover:bg-amber-300 w-[76vw]  md:w-[300px] pl-5 md:pl-6 pr-1 md:pr-[5px] mx-auto">
                         <p className="mx-auto xs:pr-2">HappyHacksについて</p>
-                        <span className="px-2.5 py-2 mr-0.5 rounded-full text-white bg-amber-400 group-hover:text-gray-700">
+                        <span className="px-[12px] py-[10px] mr-0.5 rounded-full text-white bg-amber-400 group-hover:text-gray-700">
                           →
                         </span>
                       </UnstyledButton>
