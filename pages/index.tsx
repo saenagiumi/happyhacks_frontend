5c3372faf2f433921979c4267118045f8ba42cc8
takeru.dev <takeru.konno.cw`gmail.com>
2023-03-30 10:44:06 +0900

    ✅ 本番環境でのパフォーマンス確認のため、SSRを無効にした。

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,29 @@ import { API_BASE_URL } from "const/const";
 import ActiveTab from "pages/[activeTab]";
 import { SWRConfig } from "swr";
 
-export const getServerSideProps = async () => {
-  const posts = await fetch(`${API_BASE_URL}/posts_with_comments_count`);
-  const postsData = await posts.json();
+// export const getServerSideProps = async () => {
+//   const posts = await fetch(`${API_BASE_URL}/posts_with_comments_count`);
+//   const postsData = await posts.json();
 
-  return {
-    props: {
-      fallback: {
-        [`${API_BASE_URL}/posts_with_comments_count`]: postsData,
-      },
-    },
-  };
-};
+//   return {
+//     props: {
+//       fallback: {
+//         [`${API_BASE_URL}/posts_with_comments_count`]: postsData,
+//       },
+//     },
+//   };
+// };
 
-export default function Home(props: any) {
-  const { fallback } = props;
+export default function Home(
+  // props: any
+  ) {
+  // const { fallback } = props;
 
   return (
     <>
-      <SWRConfig value={{ fallback }}>
+      {/* <SWRConfig value={{ fallback }}> */}
         <div>{<ActiveTab />}</div>
-      </SWRConfig>
+      {/* </SWRConfig> */}
     </>
   );
 }
