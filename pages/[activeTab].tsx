a1f725d7476f39bfbdf6af73d34d9d1b9f4e4c1f
takeru.dev <takeru.konno.cw`gmail.com>
2023-03-30 18:58:34 +0900

    🎨 [Update]: ボタンをコンポーネント化した

--- pages/[activeTab].tsx
+++ pages/[activeTab].tsx
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
-import { Tabs, UnstyledButton } from "@mantine/core";
+import { Tabs } from "@mantine/core";
 import { PostsOrderByCreatedSequence } from "features/posts/components/PostsOrderByCreatedSequence";
 import { PostsOrderByCommentsLength } from "features/posts/components/PostsOrderByCommentsLength";
-import Link from "next/link";
 import Image from "next/image";
-import { RiSearch2Line } from "react-icons/ri"; 
 import { NextSeo } from "next-seo";
-export const ActiveTab = () => {
+import AboutButton from "components/AboutButton";
+import { memo } from "react";
+export const ActiveTab = memo(() => {
   const router = useRouter();
   const seo = `${
     router.asPath === "/" ? "HappyHacks" : "/recent" ? "新着 | HappyHacks" : ""
@@ -34,16 +34,8 @@ export const ActiveTab = () => {
                     <span className="inline-block font-sans">ADHDの日常生活</span>
                   </h1>
 
-                  <div className="flex">
-                    <Link href={"/about"} legacyBehavior>
-                      <UnstyledButton className="flex justify-end items-center group text-[1.1rem] xs:text-[1.1rem] md:text-[1.2rem] xs:w-[300px] h-[3.5rem] md:h-[3.5rem] text-gray-700 font-sans font-[600] border-2 border-solid border-amber-300 rounded-full bg-white hover:bg-amber-300 w-[73vw]  md:w-[300px] pl-5 md:pl-6 pr-1 md:pr-[5px] mx-auto">
-                        <RiSearch2Line />
-                        <p className="mx-auto text-[1.12rem] xs:pr-2">HappyHacksとは？</p>
-                        <span className="px-[10px] py-[8px] mr-1.5 rounded-full text-white bg-amber-400 group-hover:text-gray-700">
-                          →
-                        </span>
-                      </UnstyledButton>
-                    </Link>
+                  <div>
+                    <AboutButton />
                   </div>
                 </div>
               </div>
@@ -103,6 +95,6 @@ export const ActiveTab = () => {
       </div>
     </>
   );
-};
+});
 
 export default ActiveTab;
