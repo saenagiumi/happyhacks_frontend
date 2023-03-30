0749610d8138f41b484e75f8a4a00c132fb038c9
takeru.dev <takeru.konno.cw`gmail.com>
2023-03-30 21:02:00 +0900

    ⚡️ [Update]: 画面サイズとログイン状態によってファーストビューを変えるようにした

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,9 @@
-import { API_BASE_URL } from "const/const";
 import ActiveTab from "pages/[activeTab]";
-import { SWRConfig } from "swr";
 
-// export const getServerSideProps = async () => {
-//   const posts = await fetch(`${API_BASE_URL}/posts_with_comments_count`);
-//   const postsData = await posts.json();
-
-//   return {
-//     props: {
-//       fallback: {
-//         [`${API_BASE_URL}/posts_with_comments_count`]: postsData,
-//       },
-//     },
-//   };
-// };
-
-export default function Home(
-  // props: any
-  ) {
-  // const { fallback } = props;
-
+export default function Home() {
   return (
     <>
-      {/* <SWRConfig value={{ fallback }}> */}
-        <div>{<ActiveTab />}</div>
-      {/* </SWRConfig> */}
+      <div>{<ActiveTab />}</div>
     </>
   );
 }
