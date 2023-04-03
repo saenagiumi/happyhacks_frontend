273f45d41c74ce4a7545c697930af4152b7c8be0
takeru.dev <takeru.konno.cw`gmail.com>
2023-04-04 03:59:02 +0900

    💄 [Update]: スタイルを調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,14 @@ import { useWindowSize } from "hooks/useWindowSize";
 import HeroAria from "components/HeroAria";
 import { useDisclosure } from "@mantine/hooks";
 import PostForm from "features/posts/components/PostForm";
-import { CURRENT_LOCATION_URL } from "const/const";
+import HackList from "features/hacks/components/HackList";
+import { useState } from "react";
 
 export default function Home() {
   const { user, isLoading } = useAuth0();
-  const [width, height] = useWindowSize();
+  const [width, _] = useWindowSize();
   const router = useRouter();
+  const [activeTab, setActiveTab] = useState<string | null>("trend");
   const [opened, { open, close }] = useDisclosure(false);
   const showHero =
     width > 767 || (!isLoading && user === undefined && width < 768);
@@ -48,34 +50,65 @@ export default function Home() {
             <Tabs defaultValue="hacks" color="green.4" radius="xs">
               <div className="top-0 sticky pt-2 bg-white z-10">
                 <Tabs.List>
-                  <Tabs.Tab value="hacks" className="text-[16px] pb-3 pl-5">
-                    <span className="font-sans xs:text-[1.0rem] text-gray-700 font-[700]">
+                  <Tabs.Tab value="hacks" className="text-[16px] pb-2 pl-5">
+                    <span className="font-sans xs:text-[1rem] text-gray-700 font-[700]">
                       対策
                     </span>
                   </Tabs.Tab>
-                  <Tabs.Tab value="trend" className="text-[16px] pb-3">
+                  <Tabs.Tab value="question" className="text-[16px] pb-2">
                     <span className="font-sans xs:text-[1rem] text-gray-700 font-bold">
                       質問
                     </span>
                   </Tabs.Tab>
-                  {/* <Tabs.Tab value="unanswered" className="text-[16px] pb-3">
-                    <span className="font-sans xs:text-[1rem] text-gray-700 font-bold">
-                      未回答
-                    </span>
-                  </Tabs.Tab> */}
                 </Tabs.List>
               </div>
 
-              <Tabs.Panel value="hacks" pt="xs">
-                対策が入る
-              </Tabs.Panel>
+              <Tabs.Panel value="hacks">{/* <HackList /> */}</Tabs.Panel>
 
-              <Tabs.Panel value="trend" pt="xs">
-                <PostListTrend />
-              </Tabs.Panel>
-
-              <Tabs.Panel value="unanswered" pt="xs">
-                <PostListUnanswered />
+              <Tabs.Panel value="question">
+                <Tabs
+                  value={activeTab}
+                  onTabChange={setActiveTab}
+                  variant="pills"
+                  color="green.4"
+                  radius="xl"
+                >
+                  <div className="top-11 pt-2 sticky xs:pt-4 bg-white z-10">
+                    <Tabs.List pl="xs" pb="xs">
+                      <Tabs.Tab value="trend" className="w-[5.5rem] h-[2rem]">
+                        <span
+                          className={`flex items-center font-sans text-[0.875rem] ${
+                            activeTab !== "trend"
+                              ? "text-gray-700"
+                              : "text-white"
+                          } font-[700]`}
+                        >
+                          トレンド
+                        </span>
+                      </Tabs.Tab>
+                      <Tabs.Tab
+                        value="unanswered"
+                        className="w-[5.5rem] h-[2rem]"
+                      >
+                        <span
+                          className={`flex items-center font-sans text-[0.875rem] ${
+                            activeTab !== "unanswered"
+                              ? "text-gray-700"
+                              : "text-white"
+                          } font-[700]`}
+                        >
+                          未回答
+                        </span>
+                      </Tabs.Tab>
+                    </Tabs.List>
+                  </div>
+                  <Tabs.Panel value="trend">
+                    <PostListTrend />
+                  </Tabs.Panel>
+                  <Tabs.Panel value="unanswered">
+                    <PostListUnanswered />
+                  </Tabs.Panel>
+                </Tabs>
               </Tabs.Panel>
             </Tabs>
             <Modal
