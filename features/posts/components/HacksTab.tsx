6da520ad248b761bb4611a7dadc2ccd17538feff
takeru.dev <takeru.konno.cw`gmail.com>
2023-04-04 17:50:07 +0900

    ⚡️ [Update]: タブを横スクロール可能にした

--- features/posts/components/HacksTab.tsx
+++ features/posts/components/HacksTab.tsx
@@ -1,10 +1,10 @@
-import { Tabs } from "@mantine/core";
+import { ScrollArea, Tabs } from "@mantine/core";
 import { useState } from "react";
 
 export const HacksTabPanel = () => {
-  const [activeTab, setActiveTab] = useState<string | null>("item");
+  const [activeTab, setActiveTab] = useState<string | null>("tools");
   const TAB_ITEMS = [
-    { value: "item", label: "アイテム" },
+    { value: "tools", label: "ツール" },
     { value: "life", label: "生活" },
     { value: "job", label: "仕事" },
     { value: "learning", label: "学習" },
@@ -22,25 +22,30 @@ export const HacksTabPanel = () => {
           color="green.4"
           radius="xl"
         >
-          <div className="top-0 sticky bg-white z-10 flex items-center">
-            <Tabs.List pl="xs" py={8}>
-              {TAB_ITEMS.map((item) => (
-                <div>
-                  <Tabs.Tab value={item.value} className="w-[5rem] h-[1.8rem]">
-                    <span
-                      className={`flex items-center font-sans text-[12px] ${
-                        activeTab !== item.value
-                          ? "text-gray-400"
-                          : "text-white"
-                      } font-[600]`}
+          <ScrollArea>
+            <div className="top-0 sticky  border-gray-200 bg-white z-10 flex items-center w-[528px] xs:w-full">
+              <Tabs.List pl="xs" py={8}>
+                {TAB_ITEMS.map((item) => (
+                  <div>
+                    <Tabs.Tab
+                      value={item.value}
+                      className="w-[5rem] h-[1.8rem]"
                     >
-                      {item.label}
-                    </span>
-                  </Tabs.Tab>
-                </div>
-              ))}
-            </Tabs.List>
-          </div>
+                      <span
+                        className={`flex items-center font-sans text-[12px] ${
+                          activeTab !== item.value
+                            ? "text-gray-400"
+                            : "text-white"
+                        } font-[600]`}
+                      >
+                        {item.label}
+                      </span>
+                    </Tabs.Tab>
+                  </div>
+                ))}
+              </Tabs.List>
+            </div>
+          </ScrollArea>
 
           <>
             {TAB_ITEMS.map((item) => (
