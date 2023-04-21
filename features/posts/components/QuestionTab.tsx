43660ef9960a88af458d0072cb2eb99be824aa28
takeru.dev <takeru.konno.cw`gmail.com>
2023-04-22 06:44:03 +0900

    ✨ [Add]: 前回に開いていたタブを復元できるようにした

--- features/posts/components/QuestionTab.tsx
+++ features/posts/components/QuestionTab.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from "@mantine/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 import { PostListRecent } from "./PostListRecent";
 import { PostListTrend } from "./PostListTrend";
 import { PostListUnanswered } from "./PostListUnanswered";
@@ -7,10 +8,22 @@ import { PostListUnanswered } from "./PostListUnanswered";
 export const QuestionTabPanel = () => {
   const [activeTab, setActiveTab] = useState<string | null>("trend");
   const TAB_ITEMS = [
-    { value: "trend", label: "トレンド" },
-    { value: "recent", label: "新着" },
-    { value: "unanswered", label: "未回答" },
+    { label: "トレンド", value: "trend" },
+    { label: "新着", value: "recent" },
+    { label: "未回答", value: "unanswered" },
   ];
+
+  useEffect(() => {
+    // URLのクエリパラメータからactiveTabを復元
+    const queryParams = new URLSearchParams(window.location.search);
+    const activeTabParam = queryParams.get("activeTab");
+    if (
+      activeTabParam &&
+      TAB_ITEMS.some((item) => item.value === activeTabParam)
+    ) {
+      setActiveTab(activeTabParam);
+    }
+  }, []);
 
   return (
     <>
@@ -21,29 +34,33 @@ export const QuestionTabPanel = () => {
           variant="pills"
           radius="xl"
         >
-          <div className="top-0 sticky bg-white z-10 flex items-center">
-            <Tabs.List className="my-3.5 xs:my-0 xs:py-4 pl-3 xs:pl-5">
-              {TAB_ITEMS.map((item) => (
-                <Tabs.Tab
-                  style={{
-                    background: `${
-                      activeTab !== item.value ? "#F7F7F7" : "#42ce9f"
-                    }`,
-                  }}
-                  key={item.value}
-                  value={item.value}
-                  className="w-[5.3rem] h-[2rem] mr-0.5 xs:mr-2"
-                >
-                  <span
-                    className={`flex items-center font-sans text-[14px] xs:text-[15px] ${
-                      activeTab !== item.value ? "text-gray-500" : "text-white"
-                    } font-[600]`}
+          <div className="flex items-center">
+            <div>
+              <Tabs.List className="sticky top-0 z-10 my-3.5 bg-white pl-3 xs:my-0 xs:py-4 xs:pl-5">
+                {TAB_ITEMS.map((item) => (
+                  <Tabs.Tab
+                    style={{
+                      background: `${
+                        activeTab !== item.value ? "#F7F7F7" : "#42ce9f"
+                      }`,
+                    }}
+                    key={item.value}
+                    value={item.value}
+                    className="mr-0.5 h-[2rem] w-[5.3rem] xs:mr-2"
                   >
-                    {item.label}
-                  </span>
-                </Tabs.Tab>
-              ))}
-            </Tabs.List>
+                    <span
+                      className={`flex items-center font-sans text-[14px] xs:text-[15px] ${
+                        activeTab !== item.value
+                          ? "text-gray-500"
+                          : "text-white"
+                      } font-[600]`}
+                    >
+                      {item.label}
+                    </span>
+                  </Tabs.Tab>
+                ))}
+              </Tabs.List>
+            </div>
           </div>
 
           <>
