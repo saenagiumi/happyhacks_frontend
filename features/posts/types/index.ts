6abcdda658988d148fa31895c5a569da5bba5701
takeru.dev <takeru.konno.cw`gmail.com>
2023-04-22 06:47:08 +0900

    🏷️ [Update]: 型定義を更新した

--- features/posts/types/index.ts
+++ features/posts/types/index.ts
@@ -1,27 +1,75 @@
 export type PostType = {
   id: number;
+  name: string;
   title: string;
   body: string;
-  name: string;
+  created_at: string;
+  picture: string;
   user_id: string;
-  picture: string;
-  created_at: string;
 };
 
-export type PostWithCommentsCountType = {
-  id: number;
+export type PostReturnType = {
+  id: string;
+  name: string;
   title: string;
   body: string;
-  name: string;
-  user_id: number;
-  picture: string;
-  created_at: string;
   comments_count: number;
+  created_at: string;
+  picture: string;
+  updated_at: string;
+  user_id: number;
 };
 
+export type Hack = {
+  id: number;
+  name: string;
+  title: string;
+  body: string;
+  bookmarks_count: number;
+  category: string;
+  created_at: string;
+  picture: string;
+  tags: string[];
+  tweet_id: string;
+  user_id: number;
+};
+
+export type HackFormData = {
+  id?: string
+  title: string;
+  body: string;
+  category: string;
+  tags: string[];
+  tweet_id: string;
+};
 
 export type TargetPost = {
   id: string;
   title: string;
   body: string;
+};
+
+export type TargetHack = {
+  id: string;
+  title: string;
+  body: string;
+  category: string;
+  tags: string[];
+  tweet_id: string;
+};
+
+export type Like = {
+  id: string | undefined
+  created_at: string;
+  hack_id: number;
+  updated_at: string;
+  user_id: number;
+};
+
+export type Bookmark = {
+  id: number;
+  created_at: string;
+  hack_id: number;
+  updated_at: string;
+  user_id: number;
 };
