9e29da97013b177c494a831611f2aac4bafb503b
takeru.dev <takeru.konno.cw`gmail.com>
2023-04-22 06:32:26 +0900

    🚨 [Update]: ESLintを使ってリファクタした

--- features/comments/components/LikeButton.tsx
+++ features/comments/components/LikeButton.tsx
@@ -1,25 +1,25 @@
 import { Player } from "@lottiefiles/react-lottie-player";
-import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
-import likeHeart from "public/like-heart.json";
 import { UnstyledButton } from "@mantine/core";
+import { useRouter } from "next/router";
+import likeHeart from "public/like-heart.json";
+import { useEffect, useRef, useState } from "react";
 
 const LAST_FRAME = 89;
 
 type Props = {
   isLiked: boolean;
-  commentLikesIsLoading: boolean;
   likesCount: {
-    status: string;
     count: number;
-  };
+    status: string;
+  } | null;
+  likesIsLoading: boolean;
   onClick: () => {};
 };
 
 const LikeButton = ({
   isLiked,
-  commentLikesIsLoading,
   likesCount,
+  likesIsLoading,
   onClick,
 }: Props) => {
   const playerRef = useRef<Player>(null);
@@ -27,7 +27,7 @@ const LikeButton = ({
   const router = useRouter();
 
   useEffect(() => {
-    if (playerRef.current && !commentLikesIsLoading) {
+    if (playerRef.current && !likesIsLoading) {
       // 最初からtrueの場合はアニメーション静止
       if (isLiked === true && initialState === null) {
         playerRef.current.setSeeker(LAST_FRAME, false);
@@ -48,7 +48,7 @@ const LikeButton = ({
 
   useEffect(() => {
     // URLに変更があった場合、再レンダリング
-    if (!commentLikesIsLoading) {
+    if (!likesIsLoading) {
       // アニメーションの再生準備
       if (isLiked === false) {
         setInitialState(false);
@@ -57,31 +57,35 @@ const LikeButton = ({
         setInitialState(null);
       }
     }
-  }, [router.asPath, commentLikesIsLoading]);
+  }, [router.asPath, likesIsLoading]);
 
   return (
     <UnstyledButton onClick={onClick}>
       <div className="flex items-center">
         <div
-          className={`border-[0.5px] border-solid  ${
+          className={`border-[1px] border-solid w-[38px] h-[37.5px]  ${
             isLiked
-              ? "border-rose-300 bg-red-50"
+              ? "border-rose-200 bg-red-50"
               : "border-gray-100 bg-gray-100"
-          }   w-[38px] h-[38px] rounded-full flex justify-center items-center font-bold text-sm text-gray-400 mr-1.5`}
+          }    rounded-full flex justify-center items-center font-bold text-sm text-gray-400 ${
+            likesCount !== null ? "mr-1.5" : ""
+          }`}
         >
           <div>
             <Player
               ref={playerRef}
-              speed={2}
+              speed={1.5}
               keepLastFrame
               src={likeHeart}
               style={{ height: "40px", width: "40px" }}
             ></Player>
           </div>
         </div>
-        <div className="text-gray-400 font-bold text-sm mr-2 mt-[1px] w-[10px]">
-          {likesCount ? likesCount.count : 0}
-        </div>
+        {likesCount !== null && (
+          <div className="text-gray-400 font-bold text-sm mr-2 mt-[1px] w-[10px]">
+            {likesCount ? likesCount.count : 0}
+          </div>
+        )}
       </div>
     </UnstyledButton>
   );
--- features/posts/api/getUser.ts
+++ features/posts/api/getUser.ts
@@ -1,23 +1,12 @@
 import axios from "axios";
-import { User } from "features/users/types";
 import { API_BASE_URL } from "const/const";
+import { User } from "features/users/types";
 
 export const getPostUser = async (postId: string) => {
   try {
     const res = await axios.get<User>(`${API_BASE_URL}/posts/${postId}/user`);
     return res.data;
-  } catch (e: any) {
-    // エラー発生の状況を特定できていないので、以下は暫定的な対応
-    if (e.response.status === 401 || 403) {
-      throw new Error("Unauthorized");
-    }
-
-    let message;
-    if (axios.isAxiosError(e) && e.response) {
-      console.error(e.response.data.message);
-    } else {
-      message = String(e);
-      console.error(message);
-    }
+  } catch (error) {
+    console.error("Error in getPostUser:", error);
   }
 };
