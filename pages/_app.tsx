9eeac5a91f8af03fb4cb7cf9c4c845e331ce0747
takeru.dev <takeru.konno.cw`gmail.com>
2023-04-24 15:30:05 +0900

    📈 [Add]: GAを導入した

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,13 +9,28 @@ import {
 import { Header } from "components/Layout/Header";
 import ProgessBar from "components/Layout/ProgressBar";
 import Auth from "features/auth/components/Auth";
+import * as gtag from "lib/gtag";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import Script from "next/script";
 import { DefaultSeo } from "next-seo";
+import { useEffect } from "react";
 import { MdCheckCircle } from "react-icons/md";
 
 export default function App({ Component, pageProps }: AppProps) {
   const redirectUri = `${process.env["NEXT_PUBLIC_BASE_URL"]}`;
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
 
   const onRedirectCallback = (appState: any) => {
     showNotification({
@@ -36,6 +51,23 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
       <Head>
         <meta
           name="viewport"
