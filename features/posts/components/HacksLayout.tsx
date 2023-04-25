4f8a0e53ad3e329d17d3a40ff07573b699c635d5
takeru.dev <takeru.konno.cw`gmail.com>
2023-04-26 05:43:04 +0900

    ⚡️ [Update]: 表示される文字数を制限した

--- features/posts/components/HacksLayout.tsx
+++ features/posts/components/HacksLayout.tsx
@@ -6,7 +6,7 @@ import { Hack } from "../types";
 const HacksLayout = ({ hack }: { hack: Hack }) => {
   return (
     <div>
-      <div className="px-2 pr-3 pb-1 xs:px-1.5 xs:hover:bg-slate-100">
+      <div className="group px-2 pr-3 pb-1 xs:px-1.5 xs:hover:bg-slate-100">
         <div className="pt-[18px] xs:pl-10 xs:pr-20 xs:pt-10 xs:pb-6">
           <div className="mb-2 flex items-center justify-between">
             <div className="flex items-center">
@@ -41,7 +41,16 @@ const HacksLayout = ({ hack }: { hack: Hack }) => {
 
             <div className="pt-2 pb-1">
               <div className="w-full break-all text-[14px] leading-7 tracking-wide text-main-black xs:text-[1.125rem] xs:leading-8">
-                {hack.body}
+                {hack.body.length > 150 ? (
+                  <span>
+                    {`${hack.body.slice(0, 100)} `}
+                    <span className="text-[14px] font-bold text-blue-400 group-hover:underline">
+                      ...もっと読む
+                    </span>
+                  </span>
+                ) : (
+                  hack.body
+                )}
               </div>
             </div>
             <div className="mt-1 mb-3">
@@ -56,29 +65,6 @@ const HacksLayout = ({ hack }: { hack: Hack }) => {
               </div>
             </div>
           </div>
-          {/* <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <Avatar
-                alt={`${hack.name}のアイコン`}
-                src={hack.picture}
-                className="mr-2"
-                size={26}
-                radius={50}
-              />
-              <div className="font-sans text-[12px] font-bold text-gray-700">
-                {hack.name}
-              </div>
-            </div>
-
-            {hack.bookmarks_count !== 0 && (
-              <div className="flex items-center text-[14px] text-gray-500">
-                <FaRegBookmark className="mr-1" />
-                <div className="text-[16px] text-gray-600">
-                  {hack.bookmarks_count}
-                </div>
-              </div>
-            )}
-          </div> */}
         </div>
       </div>
     </div>
